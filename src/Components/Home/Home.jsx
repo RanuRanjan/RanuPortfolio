5e17e0bff7a01e63938868dce2a65d6c8f5d0fcd
RanuRanjan <ranuranjan25@gmail.com>
2021-12-03 16:44:32 +0530

    deploy

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,186 @@
+import React, { useEffect, useState } from 'react';
+import styles from './Home.module.css';
+import Card from '../Card/Card';
+import SideIcons from '../SideIcons/SideIcons';
+import TechStacks from '../TechStacks/TechStacks';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import { Button } from '../Button/Button';
+import About from '../About/About';
+import { projects } from '../../Utils/Projects';
+import Contact from '../Contact/Contact';
+import { ThemeContext } from '../../ContextProvider/ThemeContext';
+
+
+const Home = ({ scrollRef }) => {
+	const [projectArray, setProjectArray] = useState(
+		projects.slice(0, 4),
+	);
+	const { newTheme } = React.useContext(ThemeContext);
+	const [offset, setOffset] = useState(0);
+	const [location, setLocation] = useState(0);
+	useEffect(() => {
+		AOS.init();
+	});
+
+	const handleOffset = () => {
+		setOffset(window.pageYOffset);
+	};
+
+	useEffect(() => {
+		window.addEventListener('scroll', handleOffset);
+		return () => window.removeEventListener('scroll', handleOffset);
+	}, []);
+
+	useEffect(() => {
+		document.body.style.backgroundColor = newTheme.background;
+	}, [newTheme]);
+
+	const handleButton = () => {};
+
+	console.log(offset);
+	const handleShowMoreBtn = () => {
+		if (projects.length === projectArray.length) {
+			setProjectArray(projects.slice(0, 4));
+			window.scrollTo(0, location);
+		} else {
+			setProjectArray(projects);
+			setLocation(offset)
+		}
+	};
+
+	return (
+		<div ref={scrollRef}>
+			<SideIcons />
+			<div
+				id='home'
+				className={styles.profile}
+				style={{ backgroundColor: `${newTheme.imgBackground}` }}
+			>
+				<div
+					data-aos='fade-zoom-out'
+					className={styles.intro}
+					style={{
+						color: `${newTheme.para}`,
+						transform: `translateX(-${offset * 2.5}px)`,
+						opacity: `${
+							offset > 300 ? '0' : offset > 160 ? '.5' : '1'
+						}`,
+					}}
+				>
+					<h1>
+						<span>Hi, My name is</span>
+						<div
+							className={styles.name}
+							style={{ color: `${newTheme.title}` }}
+						>
+							Ranu Ranjan.
+						</div>
+					</h1>
+					<h1>
+					I'm an Aspiring full-stack developer with a specialization in MERN stack 
+					with a passion for learning with an endless supply of optimism and curiosity.
+					</h1>
+					<div className={styles.btn}>
+						<a
+							href='https://drive.google.com/file/d/1-BdmTqY-aWEIvQxX4EMtIRKKdxH05hOZ/view' // Resume
+							target='blank'
+						>
+							<Button
+								text={
+									<span className={styles.resumeBtn}>
+										<span>Resume</span>{' '}
+										<i className='fas fa-file-download'></i>
+									</span>
+								}
+								handleButton={handleButton}
+							/>
+						</a>
+					</div>
+				</div>
+
+				<div
+					style={{
+						transform: `translateX(${offset * 2.5}px)`,
+						opacity: `${
+							offset > 300 ? '0' : offset > 160 ? '.5' : '1'
+						}`,
+					}}
+					className={styles.profileImage}
+				>
+					<img
+						src='https://avatars.githubusercontent.com/u/53153822?v=4' //my Picture
+						alt='Profile pic'
+					/>
+				</div>
+			</div>
+
+			<div
+				id='about'
+				style={{
+					background: `${newTheme.highlightBackground}`,
+				}}
+				className={styles.experience}
+			>
+				<About />
+			</div>
+
+			<div id='techStacks' className={styles.techStacks}>
+				<TechStacks />
+			</div>
+
+			{/* <div
+				id='experience'
+				className={styles.experience}
+				style={{
+					background: `${newTheme.highlightBackground}`,
+				}}
+			>
+				<div
+					data-aos='fade-right'
+					data-aos-offset='150'
+					data-aos-easing='ease-in-sine'
+					data-aos-duration='700'
+					style={{ color: `${newTheme.para}` }}
+				>
+					<Experiences />
+				</div>
+			</div> */}
+
+			{/* project section from here => */}
+			<div id='projects' className={styles.projects}>
+				<h1
+					style={{ color: `${newTheme.title}` }}
+					className={styles.heading}
+				>
+					Things I've Developed
+				</h1>
+				<div className={styles.borderBottom} />
+				<div>
+					{projectArray.map((item, index) => (
+						<Card key={index} {...item} />
+					))}
+				</div>
+				<Button
+					text={
+						projects.length !== projectArray.length
+							? 'Show More'
+							: 'Show Less'
+					}
+					handleButton={handleShowMoreBtn}
+				/>
+			</div>
+
+			<div
+				style={{
+					background: `${newTheme.highlightBackground}`,
+				}}
+			>
+				<Contact />
+			</div>
+		</div>
+	);
+};
+
+export default Home;
---
+++ src/Utils/Projects.js
@@ -0,0 +1,56 @@
+export const projects = [
+	{
+		img: 'https://user-images.githubusercontent.com/53153822/143931821-e4a554d2-891b-4a92-a962-678d12a762d7.png',
+		title: 'Lynda clone',
+		des: 'Lynda is an E-learning Platform by LinkedIn. Built with HTML,CSS, and Js.Have features that allow users to buy subscriptions after creating an account on it. Also have features that users sort courses according to views count.',
+		demo: '',
+		live: 'https://linkedin-learning-clone.herokuapp.com/index', // deployed link
+		gitHub: 'https://github.com/RanuRanjan/LyndaClone.github.io',
+		technologies: [ 'HTML', 'CSS', 'JavaScript'],
+	},
+	{
+		img: 'https://user-images.githubusercontent.com/53153822/143927187-b3139b2d-6f8c-4f5e-8a6e-e88566577a51.png',
+		title: 'Tomtop Clone',
+		des: 'An E-commerce website where users can buy any electronics and home accessories products from all over the world.Have features that allow users to add products to the cart, buy them and also sort the products on the basis of price.',
+		demo: '',
+		live: 'https://ranuranjan.github.io/TomtopClone/',
+		gitHub: 'https://github.com/RanuRanjan/TomtopClone',
+		technologies: [ 'HTML', 'CSS', 'JavaScript']
+	},
+	{
+		img: 'https://user-images.githubusercontent.com/53153822/141642626-09016284-eeb1-4eec-82b9-92fc31ce5ba8.png',
+		title: 'RedBus Clone',
+		des: 'RedBus is India’s largest online bus ticketing platform that has transformed bus travel in the country by bringing ease and convenience to millions of Indians who travel using buses.It has features for login/logout . Users can search vehicles according to their needs.Users also book Outstation Vehicle . Users get ticket after successful booking.',
+		demo: '',
+		live: 'https://front-end-15ofn5qe4-ranuranjan.vercel.app',
+		gitHub: 'https://github.com/RanuRanjan/Redbus_clone/tree/master',
+		technologies: ['React', 'Redux', 'HTML', 'CSS', 'JavaScript','MongoDB'],
+	},
+	// {
+	// 	img: 'https://user-images.githubusercontent.com/73213873/107113597-f547e200-6885-11eb-8abd-72106074907a.png',
+	// 	title: 'Clone of Medium',
+	// 	des: 'Clone of a blogging web application, Medium where a user can read the blogs written by others and show their response also, one can share their thoughts by posting a blog.',
+	// 	demo: '',
+	// 	live: 'https://medium-web.netlify.app/',
+	// 	gitHub: 'https://github.com/himrd95/Medium-Clone.git',
+	// 	technologies: ['React', 'HTML', 'CSS', 'javaScript'],
+	// },
+	// {
+	// 	img: 'https://camo.githubusercontent.com/b7f4aa8c34ddf776dd19b78dc3488456470a73e5026f48784e9e753601468802/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4c62507030307771454537386c6f68584d372f67697068792e676966',
+	// 	title: 'Clone of Fitness Blender',
+	// 	des: 'This is a clone of the Fitness Blender website. The clone is complete with a Login page, Sign-in page, Shopping Cart, Wishlist and Calendar Scheduling feature. Following is the link to the clone of the website.',
+	// 	demo: '',
+	// 	live: 'https://anshusagar.github.io/FitnessBlender/',
+	// 	gitHub: 'https://github.com/Anshusagar/FitnessBlender.git',
+	// 	technologies: ['HTML', 'CSS', 'javaScript'],
+	// },
+	// {
+	// 	img: 'https://github.com/mandarsatam/Mailchimp-Clone/blob/master/Snips/Screenshot%20(79).png?raw=true',
+	// 	title: 'Mailchimp Clone',
+	// 	des: 'This is a static clone of a website called Mailchimp. This clone includes landing, pricing, our story, and the resources page.',
+	// 	demo: '',
+	// 	live: 'https://mandarsatam.github.io/Mailchimp-Clone/',
+	// 	gitHub: 'https://github.com/mandarsatam/Mailchimp-Clone',
+	// 	technologies: ['HTML', 'CSS', 'javaScript'],
+	// },
+];
