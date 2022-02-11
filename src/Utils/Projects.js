d66b55cc44446ccf64cafe64f053ff7e9fe98185
RanuRanjan <ranuranjan25@gmail.com>
2022-02-11 13:18:16 +0530

    new project added

--- src/Utils/Projects.js
+++ src/Utils/Projects.js
@@ -34,16 +34,16 @@ export const projects = [
 		live: 'weather-app-zeta-fawn.vercel.app',
 		gitHub: 'https://github.com/RanuRanjan/WeatherApp',
 		technologies: ['React', 'HTML', 'CSS', 'javaScript'],
+	},
+	{
+		img: 'https://user-images.githubusercontent.com/53153822/153553863-0c7453fe-9bb5-4720-b6d7-5591511f7bfe.jpg',
+		title: 'Calculator',
+		des: 'This calculator app where users can perform basic arithmetic operations',
+		demo: '',
+		live: 'https://ranuranjan.github.io/Calculator/',
+		gitHub: 'https://github.com/RanuRanjan/Calculator',
+		technologies: ['HTML', 'CSS', 'javaScript'],
 	}
-	// {
-	// 	img: 'https://camo.githubusercontent.com/b7f4aa8c34ddf776dd19b78dc3488456470a73e5026f48784e9e753601468802/68747470733a2f2f6d656469612e67697068792e636f6d2f6d656469612f4c62507030307771454537386c6f68584d372f67697068792e676966',
-	// 	title: 'Clone of Fitness Blender',
-	// 	des: 'This is a clone of the Fitness Blender website. The clone is complete with a Login page, Sign-in page, Shopping Cart, Wishlist and Calendar Scheduling feature. Following is the link to the clone of the website.',
-	// 	demo: '',
-	// 	live: 'https://anshusagar.github.io/FitnessBlender/',
-	// 	gitHub: 'https://github.com/Anshusagar/FitnessBlender.git',
-	// 	technologies: ['HTML', 'CSS', 'javaScript'],
-	// },
 	// {
 	// 	img: 'https://github.com/mandarsatam/Mailchimp-Clone/blob/master/Snips/Screenshot%20(79).png?raw=true',
 	// 	title: 'Mailchimp Clone',
